d9a9fc13d5e88e72a5d9cbf07aef4b43b94c0020
Mattias <duartemattias4@gmail.com>
2023-07-28 17:20:39 -0400

    new products

--- database/seeders/ProductImageSeeder.php
+++ database/seeders/ProductImageSeeder.php
@@ -14,108 +14,122 @@ class ProductImageSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('product_images')->insert([
-            'product_id' => "07d03660-a54c-4fd5-8ab8-d34ce637c867",
-            'url'      => "/storage/products/play-station-5-1.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        DB::table('product_images')->insert([
-            'product_id' => "07d03660-a54c-4fd5-8ab8-d34ce637c867",
-            'url'      => "/storage/products/play-station-5-2.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // DB::table('product_images')->insert([
+        //     'product_id' => "07d03660-a54c-4fd5-8ab8-d34ce637c867",
+        //     'url'      => "/storage/products/play-station-5-1.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('product_images')->insert([
+        //     'product_id' => "07d03660-a54c-4fd5-8ab8-d34ce637c867",
+        //     'url'      => "/storage/products/play-station-5-2.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "7eb79d7e-3bba-4a3f-9974-e50d1a661c68",
+        //     'url'      => "/storage/products/macbook-pro-m2-1.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "7eb79d7e-3bba-4a3f-9974-e50d1a661c68",
+        //     'url'      => "/storage/products/macbook-pro-m2-2.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "7eb79d7e-3bba-4a3f-9974-e50d1a661c68",
+        //     'url'      => "/storage/products/macbook-pro-m2-3.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "872b0f16-32b9-426d-9bce-1073a73ca969",
+        //     'url'      => "/storage/products/iphone-14-1.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "872b0f16-32b9-426d-9bce-1073a73ca969",
+        //     'url'      => "/storage/products/iphone-14-2.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "9160d376-f4af-4b51-b484-c98b9a3343b9",
+        //     'url'      => "/storage/products/iphone-14-pro-max-1.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "9160d376-f4af-4b51-b484-c98b9a3343b9",
+        //     'url'      => "/storage/products/iphone-14-pro-max-2.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "d7a940a1-1db2-4835-ad1e-ab296e6d6a8e",
+        //     'url'      => "/storage/products/airpods-1.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "d7a940a1-1db2-4835-ad1e-ab296e6d6a8e",
+        //     'url'      => "/storage/products/airpods-2.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "d7a940a1-1db2-4835-ad1e-ab296e6d6a8e",
+        //     'url'      => "/storage/products/airpods-3.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "fae7143c-ddf8-4396-9f0f-f3f7c2fb5c66",
+        //     'url'      => "/storage/products/samsung-s23-plus-1.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "fae7143c-ddf8-4396-9f0f-f3f7c2fb5c66",
+        //     'url'      => "/storage/products/samsung-s23-plus-2.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('product_images')->insert([
+        //     'product_id' => "fae7143c-ddf8-4396-9f0f-f3f7c2fb5c66",
+        //     'url'      => "/storage/products/samsung-s23-plus-3.webp",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
 
         DB::table('product_images')->insert([
-            'product_id' => "7eb79d7e-3bba-4a3f-9974-e50d1a661c68",
-            'url'      => "/storage/products/macbook-pro-m2-1.webp",
+            'product_id' => "e0443054-1082-4a87-990c-b54f501e073d",
+            'url'      => "/storage/products/xbox-series-x-1.webp",
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
-
         DB::table('product_images')->insert([
-            'product_id' => "7eb79d7e-3bba-4a3f-9974-e50d1a661c68",
-            'url'      => "/storage/products/macbook-pro-m2-2.webp",
+            'product_id' => "e0443054-1082-4a87-990c-b54f501e073d",
+            'url'      => "/storage/products/xbox-series-x-2.webp",
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "7eb79d7e-3bba-4a3f-9974-e50d1a661c68",
-            'url'      => "/storage/products/macbook-pro-m2-3.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "872b0f16-32b9-426d-9bce-1073a73ca969",
-            'url'      => "/storage/products/iphone-14-1.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "872b0f16-32b9-426d-9bce-1073a73ca969",
-            'url'      => "/storage/products/iphone-14-2.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "9160d376-f4af-4b51-b484-c98b9a3343b9",
-            'url'      => "/storage/products/iphone-14-pro-max-1.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "9160d376-f4af-4b51-b484-c98b9a3343b9",
-            'url'      => "/storage/products/iphone-14-pro-max-2.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "d7a940a1-1db2-4835-ad1e-ab296e6d6a8e",
-            'url'      => "/storage/products/airpods-1.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "d7a940a1-1db2-4835-ad1e-ab296e6d6a8e",
-            'url'      => "/storage/products/airpods-2.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "d7a940a1-1db2-4835-ad1e-ab296e6d6a8e",
-            'url'      => "/storage/products/airpods-3.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "fae7143c-ddf8-4396-9f0f-f3f7c2fb5c66",
-            'url'      => "/storage/products/samsung-s23-plus-1.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "fae7143c-ddf8-4396-9f0f-f3f7c2fb5c66",
-            'url'      => "/storage/products/samsung-s23-plus-2.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('product_images')->insert([
-            'product_id' => "fae7143c-ddf8-4396-9f0f-f3f7c2fb5c66",
-            'url'      => "/storage/products/samsung-s23-plus-3.webp",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -59,72 +59,82 @@ class ProductSeeder extends Seeder
         //     'created_at' => Carbon::now(),
         //     'updated_at' => Carbon::now()
         // ]);
+        // DB::table('products')->insert([
+        //     'id'        => Uuid::uuid4(),
+        //     'name'      => "AirPods",
+        //     'slug'      => "airpods",
+        //     'stock'     => 160,
+        //     'price'     => 79.90,
+        //     'description' => "Los AirPods Pro se han convertido en uno de los productos Apple más deseados gracias a las características que contemplan respecto a sus antecesores, los AirPods. Actualmente, estos auriculares inalámbricos se encuentran en el mercado con una versión más actualizada: los AirPods Pro 2ª Generación.",
+        //     'category_id' => "5c85d335-b504-4825-b570-60b59e26d070",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('products')->insert([
+        //     'id'        => Uuid::uuid4(),
+        //     'name'      => "IPhone 14",
+        //     'slug'      => "iphone-14",
+        //     'stock'     => 60,
+        //     'price'     => 499.00,
+        //     'description' => "El Apple iPhone 14 llega este año con el mismo chip A15 Bionic del iPhone 13. Con una pantalla de 6.1 pulgadas, el iPhone 14 cuenta con una cámara dual con dos sensores de 12MP, siendo el principal estabilizado. La cámara selfie es de 12MP y suma PDAF y la batería puede cargarse en forma inalámbrica mediante MagSafe o por cable, logrando una carga de 50% en 30 minutos.",
+        //     'category_id' => "89738397-4674-423f-b607-a0763eee5f61",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('products')->insert([
+        //     'id'        => Uuid::uuid4(),
+        //     'name'      => "IPhone 14 Pro Max",
+        //     'slug'      => "iphone-14-pro-max",
+        //     'stock'     => 20,
+        //     'price'     => 699.00,
+        //     'description' => "El Apple iPhone 14 Pro Max es el iPhone más grande y poderoso de la serie 14. Potenciado por el nuevo procesador Apple A16 Bionic, el iPhone 14 Pro Max cuenta con una pantalla OLED LTPO de 6.7 pulgadas con soporte always-on y un nuevo notch en forma de píldora que se integra a iOS con las notificaciones. La cámara trasera es triple y la cámara principal del conjunto es de 48MP con estabilización por sensor, sumando cámaras ultrawide y telefoto de 12MP",
+        //     'category_id' => "89738397-4674-423f-b607-a0763eee5f61",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('products')->insert([
+        //     'id'        => Uuid::uuid4(),
+        //     'name'      => "MacBookPro M2",
+        //     'slug'      => "macbook-pro-m2",
+        //     'stock'     => 20,
+        //     'price'     => 750.00,
+        //     'description' => "la laptop MacBook Pro de 13 pulgadas es portátil y potente. Es más rápida que una CPU de 8 núcleos de próxima generación, tiene una unidad de procesamiento de gráficos de 10 núcleos y hasta 24 GB de memoria unificada. Batería de hasta 20 horas de duración: puedes usarla todo el día hasta la noche, gracias al rendimiento eficiente del chip M2 de Apple. Rendimiento sostenible gracias a su sistema de refrigeración activa, la MacBook Pro de 13 pulgadas puede mantener niveles profesionales de rendimiento para que puedas ejecutar tareas exigentes de la CPU y la GPU durante horas y horas.",
+        //     'category_id' => "9c3486dc-b1b3-4a10-b5a4-91413cef0fe5",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('products')->insert([
+        //     'id'        => Uuid::uuid4(),
+        //     'name'      => "Play Station 5",
+        //     'slug'      => "play-station-5",
+        //     'stock'     => 65,
+        //     'price'     => 499.90,
+        //     'description' => "La nueva consola de Sony se encuentra disponible en dos versiones: la PS5 (con lectora de discos) y la PS5 Digital Edition. Además, de una serie de accesorios que harán tu experiencia de juego fantástica. Definitivamente, estamos hablando de una máquina fuera de serie",
+        //     'category_id' => "006fe0af-379f-47cd-bb0f-769fadd2ac36",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('products')->insert([
+        //     'id'        => Uuid::uuid4(),
+        //     'name'      => "Samsung S23 Plus",
+        //     'slug'      => "samsung-s23-plus",
+        //     'stock'     => 34,
+        //     'price'     => 300.00,
+        //     'description' => "El Samsung Galaxy S23+ cuenta con una pantalla Dynamic AMOLED 2X de 6.6 pulgadas a resolución FHD+ y tasa de refresco de 120Hz y está potenciado por un procesador Snapdragon 8 Gen 2 optimizado especialmente para la serie, junto con 8GB de RAM y hasta 512GB de almacenamiento. En su posterior encontramos una cámara triple con sensor principal de 50MP con OIS, cámara ultrawide de 12MP y cámara telefoto de 10MP con zoom óptico 3x, mientras que la cámara selfie es de 12MP. Alimentado por una batería de 4700 mAh",
+        //     'category_id' => "89738397-4674-423f-b607-a0763eee5f61",
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
         DB::table('products')->insert([
             'id'        => Uuid::uuid4(),
-            'name'      => "AirPods",
-            'slug'      => "airpods",
-            'stock'     => 160,
-            'price'     => 79.90,
-            'description' => "Los AirPods Pro se han convertido en uno de los productos Apple más deseados gracias a las características que contemplan respecto a sus antecesores, los AirPods. Actualmente, estos auriculares inalámbricos se encuentran en el mercado con una versión más actualizada: los AirPods Pro 2ª Generación.",
-            'category_id' => "5c85d335-b504-4825-b570-60b59e26d070",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        DB::table('products')->insert([
-            'id'        => Uuid::uuid4(),
-            'name'      => "IPhone 14",
-            'slug'      => "iphone-14",
-            'stock'     => 60,
-            'price'     => 499.00,
-            'description' => "El Apple iPhone 14 llega este año con el mismo chip A15 Bionic del iPhone 13. Con una pantalla de 6.1 pulgadas, el iPhone 14 cuenta con una cámara dual con dos sensores de 12MP, siendo el principal estabilizado. La cámara selfie es de 12MP y suma PDAF y la batería puede cargarse en forma inalámbrica mediante MagSafe o por cable, logrando una carga de 50% en 30 minutos.",
-            'category_id' => "89738397-4674-423f-b607-a0763eee5f61",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        DB::table('products')->insert([
-            'id'        => Uuid::uuid4(),
-            'name'      => "IPhone 14 Pro Max",
-            'slug'      => "iphone-14-pro-max",
+            'name'      => "Xbox Series X",
+            'slug'      => "xbox-series-x",
             'stock'     => 20,
-            'price'     => 699.00,
-            'description' => "El Apple iPhone 14 Pro Max es el iPhone más grande y poderoso de la serie 14. Potenciado por el nuevo procesador Apple A16 Bionic, el iPhone 14 Pro Max cuenta con una pantalla OLED LTPO de 6.7 pulgadas con soporte always-on y un nuevo notch en forma de píldora que se integra a iOS con las notificaciones. La cámara trasera es triple y la cámara principal del conjunto es de 48MP con estabilización por sensor, sumando cámaras ultrawide y telefoto de 12MP",
-            'category_id' => "89738397-4674-423f-b607-a0763eee5f61",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        DB::table('products')->insert([
-            'id'        => Uuid::uuid4(),
-            'name'      => "MacBookPro M2",
-            'slug'      => "macbook-pro-m2",
-            'stock'     => 20,
-            'price'     => 750.00,
-            'description' => "la laptop MacBook Pro de 13 pulgadas es portátil y potente. Es más rápida que una CPU de 8 núcleos de próxima generación, tiene una unidad de procesamiento de gráficos de 10 núcleos y hasta 24 GB de memoria unificada. Batería de hasta 20 horas de duración: puedes usarla todo el día hasta la noche, gracias al rendimiento eficiente del chip M2 de Apple. Rendimiento sostenible gracias a su sistema de refrigeración activa, la MacBook Pro de 13 pulgadas puede mantener niveles profesionales de rendimiento para que puedas ejecutar tareas exigentes de la CPU y la GPU durante horas y horas.",
-            'category_id' => "9c3486dc-b1b3-4a10-b5a4-91413cef0fe5",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        DB::table('products')->insert([
-            'id'        => Uuid::uuid4(),
-            'name'      => "Play Station 5",
-            'slug'      => "play-station-5",
-            'stock'     => 65,
-            'price'     => 499.90,
-            'description' => "La nueva consola de Sony se encuentra disponible en dos versiones: la PS5 (con lectora de discos) y la PS5 Digital Edition. Además, de una serie de accesorios que harán tu experiencia de juego fantástica. Definitivamente, estamos hablando de una máquina fuera de serie",
+            'price'     => 500.00,
+            'description' => "Descarga y juega más de 100 juegos de alta calidad, incluidos todos los títulos nuevos de Xbox Game Studios como Halo Infinite el mismo día de su lanzamiento, con Xbox Game Pass Ultimate (la membresía se vende por separado). Xbox Entrega inteligente garantiza que juegues la mejor versión disponible de tu juego sin importar en qué consola estés jugando. En tu tienda en línea conoce lo fácil que es comprar sin salir de casa. Da clic y adquiere tus videojuegos favoritos.",
             'category_id' => "006fe0af-379f-47cd-bb0f-769fadd2ac36",
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
-        DB::table('products')->insert([
-            'id'        => Uuid::uuid4(),
-            'name'      => "Samsung S23 Plus",
-            'slug'      => "samsung-s23-plus",
-            'stock'     => 34,
-            'price'     => 300.00,
-            'description' => "El Samsung Galaxy S23+ cuenta con una pantalla Dynamic AMOLED 2X de 6.6 pulgadas a resolución FHD+ y tasa de refresco de 120Hz y está potenciado por un procesador Snapdragon 8 Gen 2 optimizado especialmente para la serie, junto con 8GB de RAM y hasta 512GB de almacenamiento. En su posterior encontramos una cámara triple con sensor principal de 50MP con OIS, cámara ultrawide de 12MP y cámara telefoto de 10MP con zoom óptico 3x, mientras que la cámara selfie es de 12MP. Alimentado por una batería de 4700 mAh",
-            'category_id' => "89738397-4674-423f-b607-a0763eee5f61",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
     }
 }
